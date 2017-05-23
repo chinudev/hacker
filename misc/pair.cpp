ab9c1928d588ef5418acc3e45c9b256101c2a51e
Chinu <chinumontu@yahoo.com>
2017-05-22 20:32:05 -0700

    solve pair problem.

---
+++ misc/pair.cpp
@@ -0,0 +1,61 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/* Head ends here */
+
+int pairs(vector < int > a,int k) {
+    int ans=0;
+    sort(a.begin(), a.end());
+    
+    int startIndex= 0;
+    int endIndex=0;
+    while (startIndex < a.size()) {
+        while ((a[endIndex] - a[startIndex]) < k) {
+            endIndex++;
+            if (endIndex >= a.size()) return ans;
+        }
+        if (a[endIndex] - a[startIndex] == k) {
+            ans++;
+        }
+        startIndex++;
+    }
+    
+    
+    return ans;
+}
+
+/* Tail starts here */
+int main() {
+    int res;
+    
+    int _a_size,_k;
+    cin >> _a_size>>_k;
+    cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n'); 
+    vector<int> _a;
+    int _a_item;
+    for(int _a_i=0; _a_i<_a_size; _a_i++) {
+        cin >> _a_item;
+        _a.push_back(_a_item);
+    }
+    
+    res = pairs(_a,_k);
+    cout << res;
+    
+    return 0;
+}
+
