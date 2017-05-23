8d11fd4a5fac08da6ccfa0708e977ff29da75a4e
Chinu <chinumontu@yahoo.com>
2017-05-22 20:02:32 -0700

    changed to graph based approach. I wasn't handling the case of combining countries after they have been formed.

--- graph/journey_to_moon.cpp
+++ graph/journey_to_moon.cpp
@@ -7,47 +7,58 @@
 using namespace std; 
 
 
+class CountryNode 
+{
+public: 
+    CountryNode(long long pop) : parent(nullptr), processed(false), population(pop) {}
+
+public:
+    CountryNode* parent;
+    bool processed;
+    long long population;
+};
+
 int main()
 { 
     long numPeople, numPairs;
 
     cin >> numPeople >> numPairs;
 
-    vector<long> countryPopulation;     // Tracks population for each country
-                                        // population<0 means this country has been
-                                        // sometime country have to be combined. in that 
-                                        //  case one of the country will point to another 
-                                        //  by using -ve value of pointed country's index
-
-    vector<long> people(numPeople,-1);  // Tracks country people belong to. 
-                                        // Initially people belong to no country (-1)
-
-    long countryCount=0;   // Index of next country that will be created
-                           // also count of country created so far.
+    vector<CountryNode*> people(numPeople,nullptr );  // Tracks country people belong to. 
 
 
     for (long i = 0; i < numPairs; ++i) {
         long a, b;
         cin >> a >> b;
 
-        if ((people[a] == -1) && (people[b] == -1)) {
+        if ((people[a] == nullptr) && (people[b] == nullptr)) {
             // New country
-            people[a]=countryCount;
-            people[b]=countryCount;
-            countryPopulation.push_back(2);    // create new country with 2 people
-            countryCount++;
+            //cout << "new " << a << " " << b << endl;
+            CountryNode* country = new CountryNode(2);
+            people[a]=country;
+            people[b]=country;
         } else {
-            if ((people[a] != -1) && (people[b] != -1)) {
-                // country are being combined 
-                assert(people[a] == people[b]);
-            } else if (people[a] != -1) {
-                long country = people[a];
+            if ((people[a] != nullptr) && (people[b] != nullptr)) {
+                // Countries are being combined. No increase in population
+                //cout << "connect " << a << " " << b << endl;
+                CountryNode* countryA=people[a];
+                while (countryA->parent != nullptr) countryA = countryA->parent;
+                CountryNode* countryB=people[b];
+                while (countryB->parent != nullptr) countryB = countryB->parent;
+                if (countryA != countryB) {
+                    //cout << "a is parent of b" << endl;
+                    countryA->parent = countryB;
+                }
+            } else if (people[a] != nullptr) {
+                CountryNode* country = people[a];
                 people[b]=country;
-                countryPopulation[country]++;
-            } else if (people[b] != -1) {
-                long country = people[b];
+                country->population++;
+                //cout << "add " << b << " to " << a << endl;
+            } else if (people[b] != nullptr) {
+                CountryNode* country = people[b];
                 people[a]=country;
-                countryPopulation[country]++;
+                country->population++;
+                //cout << "add " << a << " to " << b << endl;
             } else {
                 assert(1);  // should never reach here
             }
@@ -57,23 +68,49 @@ int main()
 
     // answer we are looking for
     long long numCombos=0;
-
     // we will keep reducing pendingPopulation as we process each country
     long long pendingPopulation=numPeople;
 
-    // Iterate through all countries 
-    for (long i=0; i < countryCount; i++) {
-        //cout << "country " << i << " " << countryPopulation[i] << endl;
-        pendingPopulation -= countryPopulation[i];
-        numCombos += pendingPopulation*countryPopulation[i];
-    }
+
+    //  find a list of ultimate country nodes
+    //  At the same time process people that don't belong to any country
+    vector <CountryNode*> rootCountries;
+    
     // iterate through people to find single person countries
+    //cout << "iterating people " << numPeople << endl;
     for (long i=0; i < numPeople ; i++) {
-        //cout << "people " << i << " " <<  people[i] << endl;
-        if (people[i] == -1) {
+        if (people[i] == nullptr) {
+            //cout << "people " << i << " " << pendingPopulation << " " << numCombos << endl;
             pendingPopulation--;
             numCombos += pendingPopulation;
+        } else {
+            CountryNode *country = people[i];
+            // processed = true for root node if it has been placed in rootCountries
+            // processed = true for leafe node if it's population has been added to root country
+            if (country->processed == false) {
+                country->processed=true;
+                if (country->parent == nullptr) {
+                    rootCountries.push_back(country);
+                } else {
+                    CountryNode* root = country;
+                    while (root->parent != nullptr) root=root->parent; 
+                    root->population += country->population;
+                    if (root->processed == false) {
+                        rootCountries.push_back(root);
+                        root->processed = true;
+                    }
+                }
+            }
         }
+    }
+
+
+    //cout << "iterating country " << endl;
+    // Iterate through all countries 
+    for (CountryNode* country : rootCountries) {
+        //cout << "country " << country->population << " pending " << pendingPopulation << " " << numCombos << endl;
+        pendingPopulation -= country->population;
+        numCombos += pendingPopulation*country->population;
     }
 
     cout << numCombos << endl;
@@ -91,3 +128,15 @@ int main()
 3 4 
 4 0
  */
+/*
+9 8
+0 1
+2 3
+4 5
+6 7 
+1 2
+5 6
+0 7
+2 6
+
+*/
