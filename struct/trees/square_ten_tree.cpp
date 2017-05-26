296f14e0ae8ee6ec0e5625896e68bc8348ec6596
Chinu <chinumontu@yahoo.com>
2017-05-26 13:52:00 -0700

    Re-implemented from scratch and added some testcases. But still failing pretty much all the test online

--- struct/trees/square_ten_tree.cpp
+++ struct/trees/square_ten_tree.cpp
@@ -44,102 +44,150 @@ string adjustLeftString(const string number, int length)
 }
 
 
-void findMinSequence(
-        const string leftNumIn, 
-        const string rightNum, 
-        vector<SeqNode>& seqVector) 
+vector<pair<int,int>> findMinSequence( const string leftNum, const string rightNum) 
 {
-    string leftNum = adjustLeftString(leftNumIn, rightNum.length());
-    assert(leftNum.length() == rightNum.length());
+    vector<pair<int,int>> seqVector;
+    //cout << "Processing " << leftNum << " " << rightNum << endl;
 
-    // Split work into two segments e.g. 
-    //   0444 - 5886 = 0444-5000 and 5000-5886
-    //
-    string midNum(rightNum.length(),'0');
-    for (int index=0; index < rightNum.length(); index++) {
-        midNum[index] = rightNum[index];
-        if (leftNum[index] != rightNum[index])  break;
-    }
+    int offset = rightNum.length() - leftNum.length();
+    int leftIndex = leftNum.length()-1;
+    int rightIndex = rightNum.length()-1;
 
-    /*
-    cout << "Left  " << leftNum << endl;
-    cout << "Mid   " << midNum << endl;
-    cout << "Right " << rightNum << endl;
-    */
 
-    // Process digits from right to left to move from leftNum to midNum
-    // for each digit position we find number movements required to 
-    //   move leftNum to match midNum
-    //  
-    int carry=0;
-    for (int index=midNum.length(); index >= 0; index--) {
-
-        // level = 0 for ones, 1 for tens, 2 for hundreds etc.
-        int currLevel = midNum.length() - 1 - index;
-        int midDigit = midNum[index] - '0';
-        int leftDigit = carry + (leftNum[index] - '0');
-
-        if (leftDigit == midDigit) {
-            // no operations needed since this digit is same for leftNum and rightNum
-            carry=0;
-        } else {
-            if (leftDigit < midDigit) {
-                carry=0;
-                seqVector.push_back(SeqNode(currLevel, midDigit-leftDigit));
+    // MSDdiff is the most significant digit that is different between the 
+    //  two numbers. For 9 1234, it is digit 0. For 1230 1234, it is digit 3
+    int MSDdiff=-1; 
+    if (offset > 0) {
+        MSDdiff=0;
+    } else {
+        for (int i=0; i<rightNum.length(); i++) {
+            if (leftNum[i] != rightNum[i]) {
+                MSDdiff=i; 
                 break;
-            } else {
-                if (midDigit != 0) cout << index << endl;
-                assert(midDigit == 0);
+            }
+        }
+    }
+    // MSD diff == -1 means two nums are same
+    //cout << "   MSD digit = " << MSDdiff << " offset " << offset << endl;
 
+    if (MSDdiff == -1) {
+        //cout << "   Operation level 0 count 1" << endl;
+        seqVector.push_back({0,1});
+        return seqVector;
+    }
+    assert(MSDdiff >= 0);
+
+    int carry=0;
+    int level=0;
+
+    // go from right to left, switching leftNum digits to 0
+    for (; rightIndex > MSDdiff; leftIndex--, rightIndex--, level++) {
+        int leftDigit = carry;
+        if (leftIndex >= 0) leftDigit += leftNum[leftIndex] - '0';
+
+        int count;
+        if (level == 0) {
+            if (leftDigit < 2) {
+                if (leftDigit == 0) count=1;
+                if (leftDigit == 1) count=0;
+                carry=0;
+            } else {
+                assert(leftDigit < 10);
                 carry=1;
-                if (leftDigit < 10) seqVector.push_back(SeqNode(currLevel, 10-leftDigit));
+                count=11-leftDigit;
             }
-        }
-    }
-
-    // Process digits from left to right to move from midNum to rightNum
-    //  
-    for (int index=0; index < rightNum.length(); index++) {
-
-        // level = 0 for ones, 1 for tens, 2 for hundreds etc.
-        int currLevel = midNum.length() - 1 - index;
-
-        int midDigit = midNum[index] - '0';
-        int rightDigit = rightNum[index] - '0';
-
-        if (midDigit == rightDigit) {
-            // no operations needed since this digit is same for leftNum and rightNum
         } else {
-            assert(midDigit < rightDigit);
-            if (seqVector.back().level == currLevel) {
-                seqVector.back().count += rightDigit-midDigit;
+            if (leftDigit == 0) {
+                carry=0;
+                count=0;
             } else {
-                seqVector.push_back(SeqNode(currLevel, rightDigit-midDigit));
+                carry=1;
+                count=10-leftDigit;
+            }
+        }
+        //cout << "   Operation level " << level << " count " << count << " carry " << carry << endl;
+        if (count > 0) seqVector.push_back({level,count});
+    }
+
+
+    // Handle MSD digit 
+    {
+        int leftDigit = carry;
+        if (leftIndex >= 0) leftDigit += leftNum[leftIndex] - '0';
+        if (level == 0) leftDigit--;
+        int rightDigit = rightNum[rightIndex] - '0';
+        int count = rightDigit-leftDigit;
+
+        assert(count >= 0);
+        //cout << "   MSD Operation level " << level << " count " << count << endl;
+        if (count > 0) seqVector.push_back({level,count});
+        rightIndex++; leftIndex++; level--;
+    }
+
+    // go left to right, going from 0 to digit in rightNum
+    for (; rightIndex < rightNum.length(); leftIndex++, rightIndex++, level--) {
+        int rightDigit = rightNum[rightIndex] - '0';
+
+        //cout << "   Operation level " << level << " count " << rightDigit << endl;
+        if (rightDigit > 0) { 
+            if (seqVector.back().first == level) {
+                //cout << "********combined*****" << endl;
+                seqVector.back().second += rightDigit;
+            } else {
+                seqVector.push_back({level,rightDigit});
             }
         }
     }
+
+
+    return seqVector;
 }
 
 void test() 
 {
+    /*
     cout << "100 " << adjustLeftString("100",3) << endl;
     cout << "100 " << adjustLeftString("100",5) << endl;
     cout << "99 " << adjustLeftString("99",2) << endl;
     cout << "99 " << adjustLeftString("99",5) << endl;
+    */
 
+
+    // Single element special case
+    assert(findMinSequence("1", "1") == (vector<pair<int,int>>{{0,1}}));
+    assert(findMinSequence("22222231", "22222231") == (vector<pair<int,int>>{{0,1}}));
+
+    // level 0 operations
+    assert(findMinSequence("0", "9") == (vector<pair<int,int>>{{0,10}}));
+    assert(findMinSequence("0", "10") == (vector<pair<int,int>>{{0,1},{1,1}}));
+    assert(findMinSequence("0", "11") == (vector<pair<int,int>>{{0,1},{1,1},{0,1}}));
+    assert(findMinSequence("1", "9") == (vector<pair<int,int>>{{0,9}}));
+    assert(findMinSequence("1", "10") == (vector<pair<int,int>>{{1,1}}));
+    assert(findMinSequence("1", "11") == (vector<pair<int,int>>{{1,1},{0,1}}));
+
+    assert(findMinSequence("2", "12") == (vector<pair<int,int>>{{0,11}}));
+    assert(findMinSequence("2", "22") == (vector<pair<int,int>>{{0,9},{1,1},{0,2}}));
+    assert(findMinSequence("12", "22") == (vector<pair<int,int>>{{0,11}}));
+
+    assert(findMinSequence("5", "1422") == (vector<pair<int,int>>{{0,6},{1,9},{2,13},{1,2},{0,2}}));
+    assert(findMinSequence("42", "1024") == (vector<pair<int,int>>{{0,9},{1,5},{2,9},{1,2},{0,4}}));
+
+    assert(findMinSequence("22222231", "22222240") == (vector<pair<int,int>>{{1,1}}));
 }
 
 int main() 
 {
 
+    //test();
+
     string leftNum, rightNum;
     cin >> leftNum >> rightNum;
 
-    vector<SeqNode> resultVector;
-    findMinSequence(leftNum, rightNum, resultVector);
+    vector<pair<int,int>> resultVector = findMinSequence(leftNum, rightNum);
 
     cout << resultVector.size() << endl;
-    for (SeqNode entry : resultVector) {
-        cout << entry.level << " " << entry.count << endl;
+    for (auto entry : resultVector) {
+        cout << entry.first << " " << entry.second << endl;
     }
+
 }
