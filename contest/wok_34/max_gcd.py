d2cab5c7f03ec9ec436a7bd496bfca6627a8ba20
Chinu <chinumontu@yahoo.com>
2017-07-19 21:30:57 -0700

    Even more brute force solution and it works now !! I can't figure out what worse case scenarios problem setter decides to use.

--- contest/wok_34/max_gcd.py
+++ contest/wok_34/max_gcd.py
@@ -3,124 +3,65 @@ import math
 
 MaxN = 10**6 
 
-class PrimeStore:
-    def __init__(self, maxN):
-        "Create a prime store with primes <= sqrt(maxN)"
-
-        self.maxN = maxN
-        self.numList = [1 for i in range(0,maxN+1)]
-        self.numList[1] = 0
-
-        prevPrime = 1
-
-        for i in range(2,int(math.sqrt(maxN))+1):
-            if self.numList[i] == 0:
-                continue
-
-            # this number is a prime 
-            self.numList[prevPrime] = i   # create a linked list of prime
-            prevPrime = i
-
-            for j in range(2*i,maxN+1,i):
-                self.numList[j]=0
-
-    def isPrime(self, n):
-        if n > self.maxN: raise Exception("range exceeded")
-
-        return self.numList[n] > 0
-
-    def findFactors(self,n):
-        if n > self.maxN: raise Exception("range exceeded")
-
-        factorDict = dict()
-        maxPrimeToTest = int(math.sqrt(n))
-        prime = self.numList[1]
-
-        while prime <= maxPrimeToTest:
-            if (n % prime == 0) : 
-                count = 0
-                while (n % prime == 0) : 
-                    n = n / prime
-                    count=count+1
-                factorDict[prime] = count
-                maxPrimeToTest = int(math.sqrt(n))
-
-            # pick next prime
-            prime = self.numList[prime]
-
-        if (n != 1): 
-            factorDict[n] = 1
-
-        return factorDict
-
-            
-
-
-PStore = PrimeStore(MaxN)
-
-
-def findGcd(dictA, dictB):
-    gcd = 1
-    for prime,countA in dictA.items():
-        if prime in dictB:
-            countB = dictB[prime]
-            count = min(countA,countB)
-            gcd = gcd * (prime**count)
-    return gcd
-
 
 def findMaxGcd(listA, listB):
 
-    listA.sort(reverse=True)
-    listB.sort(reverse=True)
+    maxA = 0
+    dictA = [0 for i in range(MaxN+1)]
+    for num in listA:
+        maxA = max(num,maxA)
+        dictA[num]=1
 
-    # use the list with smallest maxNum to prepare divisorList
-    if (listA[0] > listB[0]):
-        listA,listB = listB,listA
+    maxB = 0
+    dictB = [0 for i in range(MaxN+1)]
+    for num in listB:
+        maxB = max(num,maxB)
+        dictB[num]=1
 
-    # Use listA to create list of possible GCD
-    maxA = listA[0]
+    maxNumber = max(maxA, maxB)
+    maxPossibleGcd = min(maxA, maxB)
 
-    factorDictForA = []
+    for maxGcd in range(maxPossibleGcd,2,-1):
+        matchedA = False
+        matchedB = False
 
-    for numA in listA:
-        factorDictForA.append((numA,PStore.findFactors(numA)))
+        for gcdMultiple in range(maxGcd, maxNumber+1, maxGcd):
+            if (matchedA == False) and dictA[gcdMultiple]:
+                    matchedA = True
+                    maxMatchA = gcdMultiple
+                    for num in range(gcdMultiple, maxNumber+1, maxGcd):
+                        if (dictA[num]):
+                            maxMatchA = num
+
+                    if matchedB:
+                        #print(maxGcd, maxMatchA+maxMatchB)
+                        return maxMatchA + maxMatchB
+
+            if matchedB == False:
+                if dictB[gcdMultiple]:
+                    matchedB = True
+                    maxMatchB = gcdMultiple
+                    for num in range(gcdMultiple, maxB+1, maxGcd):
+                        if (dictB[num]):
+                            maxMatchB = num
+
+                    if matchedA:
+                        #print(maxGcd, maxMatchA+maxMatchB)
+                        return maxMatchA + maxMatchB
 
 
-    # Go through listB 
-    maxGcd = 0
-    maxGcdSum = 0
-
-    for numB in listB:
-        if (numB <= maxGcd): break
-        dictB = PStore.findFactors(numB)
-
-        for (numA,dictA) in factorDictForA:
-            if (numA < maxGcd): break
-            gcd = findGcd(dictA, dictB)
-            if (gcd > maxGcd):
-                maxGcd = gcd
-                maxGcdSum = numA + numB
-            elif (gcd == maxGcd):
-                maxGcdSum = max(maxGcdSum, numA + numB)
-
-
-    #print(maxGcd, maxGcdSum)
-    return maxGcdSum
+    # if we fall here, max gcd is 1
+    return maxA + maxB 
     
 
 def unitTest():
-    assert(PStore.findFactors(10) == {2:1, 5:1})
-    assert(PStore.findFactors(7) == {7:1})
-    assert(PStore.findFactors(64) == {2:6})
-    assert(PStore.findFactors(98) == {2:1,7:2})
 
-    findMaxGcd([8,4,3,2,1],[12,8,5,3,2])
+    print(findMaxGcd([8,4,3,2,1],[12,8,5,3,2]))
     #findMaxGcd([12,8,5,3,2],[8,4,3,2,1])
     #findMaxGcd([12,8,5,3,2],[12,8,5,3,2])
 
 
-unitTest()
+#unitTest()
 
 if __name__ == "__main__":
     n = int(input().strip())
