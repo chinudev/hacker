1ebf17266ed4b39fe9d9710600f2eb27b51e769a
Chinu <chinumontu@yahoo.com>
2017-07-20 11:17:23 -0700

    more efficient approach that fails a lot of test cases but at least no times out.

--- contest/wok_34/same_occur.py
+++ contest/wok_34/same_occur.py
@@ -2,34 +2,114 @@
 
 MaxN = 8000
 
+def createDict(arr):
+    arrDict = dict()
 
-def findCount(arr, x, y):
+    for i in range(len(arr)):
+        num = arr[i]
+        if (num in arrDict):
+            arrDict[num].append(i)
+        else:
+            arrDict[num] = [i]
+
+    marker = len(arr)
+    for key in arrDict:
+        arrDict[key].append(marker)
     
-    trailingCount = [0 for i in range(MaxN*2+1)]
-    zeroOffset = MaxN
+    return arrDict
+
+
+DictCache = dict()
+
+def findCount(arr, arrDict, x, y):
+
+    if (x not in arrDict): x = 0
+    if (y not in arrDict): y = 0
+    if (y < x): x,y = y,x
+
+    xyKey = str(x) + "." + str(y)
+    if xyKey in DictCache:
+        return DictCache[xyKey]
 
     runningCount = 0
 
-    for num in arr:
-        trailingCount[zeroOffset] += 1
-        if num == x: 
+    if (x == 0 ):
+        xList = [len(arr)]
+    else:
+        xList = arrDict[x]
+
+    if (y == 0):
+        yList = [len(arr)]
+    else:
+        yList = arrDict[y]
+
+    maxExcursion = max(len(xList), len(yList)) 
+    trailingCount = [0 for i in range(maxExcursion*2+1)]
+    zeroOffset = maxExcursion
+
+
+    xIndex = 0
+    yIndex = 0
+    currIndex = 0
+
+    while currIndex < len(arr): 
+        #print("looping ", currIndex, xIndex, yIndex, runningCount, trailingCount, zeroOffset)
+        if (currIndex == xList[xIndex]):
+            trailingCount[zeroOffset] += 1
             zeroOffset += 1
-        if num == y: 
+            runningCount += trailingCount[zeroOffset]
+
+            currIndex += 1
+            xIndex += 1
+
+        elif (currIndex == yList[yIndex]):
+            trailingCount[zeroOffset] += 1
             zeroOffset -= 1
+            runningCount += trailingCount[zeroOffset]
 
-        runningCount += trailingCount[zeroOffset]
+            currIndex += 1
+            yIndex += 1
 
+        else:
+            minNext = min(xList[xIndex],yList[yIndex])
+            nonXYCount = minNext - currIndex 
+
+            # zeroOffset doesn't change
+            adder = int((nonXYCount*(nonXYCount+1))/2) + nonXYCount*trailingCount[zeroOffset]
+            #print(" handling zeros ", minNext, nonXYCount,adder, trailingCount, zeroOffset)
+            runningCount += adder
+            trailingCount[zeroOffset] += nonXYCount
+
+            currIndex = minNext
+
+
+    DictCache[xyKey] = runningCount
     return runningCount
 
 
+def unitTest():
+    arr = [1,1,1, 4, 1,1, 4, 1,1, 5, 1,1, 5, 1,1,1]
+    arrDict = createDict(arr)
+    print(findCount(arr,arrDict, 4,5))
+    print(findCount(arr,arrDict, 4,5))
+    print(findCount(arr,arrDict, 1,4))
+    print(findCount(arr,arrDict, 1,5))
+    print(findCount(arr,arrDict, 3,3))
+    print(findCount(arr,arrDict, 6,1))
+    print(findCount(arr,arrDict, 9,1))
+    print(findCount(arr,arrDict, 1,7))
+
 
 if __name__ == "__main__":
+    unitTest()
+
     n, q = input().strip().split(' ')
     n, q = [int(n), int(q)]
     arr = list(map(int, input().strip().split(' ')))
+    arrDict = createDict(arr)
     for a0 in range(q):
         x, y = input().strip().split(' ')
         x, y = [int(x), int(y)]
-        print(findCount(arr, x, y))
+        print(findCount(arr, arrDict, x, y))
 
 
